4b90323f8bf6fb99b04155ae2367bb374d6dc8f0
mrcloud <mister.cloud@yandex.ru>
2023-10-05 15:16:42 +0300

    feat: Стилизация отображения элементов

--- frontvue/src/components/ToDoItem.vue
+++ frontvue/src/components/ToDoItem.vue
@@ -1,15 +1,47 @@
 <script setup>
+import SupportIcon from './icons/IconSupport.vue'
+
 defineProps(['value'])
 defineEmits(['remove'])
 </script>
 
 <template>
-  <li>
-    {{ value }}
-    <button @click="$emit('remove')">Remove</button>
-  </li>
+  <div class="item">
+    <i @click="$emit('remove')">
+      <SupportIcon />
+    </i>
+    <div class="details">
+      {{ value }}
+    </div>
+  </div>
 </template>
 
 <style scoped>
+  .item {
+    display: flex;
+    position: relative;
+  }
 
+  .details {
+    flex: 1;
+    margin-left: 1rem;
+    font-size: 1.2rem;
+    font-weight: 500;
+    margin-bottom: 0.4rem;
+    color: var(--color-heading);
+  }
+
+  i {
+    display: flex;
+    place-items: center;
+    place-content: center;
+    width: 32px;
+    height: 32px;
+
+    color: var(--color-text);
+
+    &:hover {
+      color: red;
+    }
+  }
 </style>
